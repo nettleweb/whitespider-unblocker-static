d45bda90c1da7e125a00e6e739b0584b6c98a807
Ruochen Jia <rjia@rjia.gq>
2022-09-28 23:12:22 +0100

    update

--- app.js
+++ app.js
@@ -2,16 +2,13 @@
 self.app = {
 	debug: true,
 	cacheName: "whitespider-unblocker",
-	cacheVersion: "0.1.1",
+	cacheVersion: "0.1.2",
 	cacheList: [],
 	headers: {
 		"Access-Control-Allow-Origin": "*",
 		"Access-Control-Allow-Methods": "GET",
 		"Access-Control-Allow-Credentials": "true",
-		//"Cross-Origin-Embedder-Policy": "require-corp",
-		//"Cross-Origin-Opener-Policy": "same-origin",
 		"Referrer-Policy": "no-referrer",
-		"X-Content-Type-Options": "nosniff",
-		//"X-Frame-Options": "SAMEORIGIN"
+		"X-Content-Type-Options": "nosniff"
 	}
 };
