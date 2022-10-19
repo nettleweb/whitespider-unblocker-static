96ac0842793af3c48dc3c6434fab59bccca2087c
Ruochen Jia <rjia@rjia.gq>
2022-10-19 18:45:08 +0100

    fix some issues

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 self.app = {
 	debug: true,
 	cacheName: "whitespider-unblocker",
-	cacheVersion: "0.6.0",
+	cacheVersion: "0.6.1",
 	cacheList: [],
 	headers: {
 		"Cross-Origin-Embedder-Policy": "require-corp",
